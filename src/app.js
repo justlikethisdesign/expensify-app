7fddc08a44ac6a71d1b2a8b8ae161836dc80006b
Rob Marshall <rob@fox.agency>
2018-09-17 15:33:49 +0100

    Removed old app components

--- src/app.js
+++ src/app.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import IndecisionApp from './components/IndecisionApp';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const Layout = (props) => {
-        return (
-        <div>
-            <p>header</p>
-            {props.children}
-            <p>footer</p>
-        </div>
-        );
-};
-
-ReactDOM.render( <IndecisionApp />, document.getElementById('app') );
+ReactDOM.render( <p>This is my boilerplate</p>, document.getElementById('app') );
