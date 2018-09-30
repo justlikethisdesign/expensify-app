6cd0cbc0337b550020cd2ad8ef28db3d5ad8b8f4
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-09-30 19:50:47 +0100

    Started adding filtering

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -130,13 +130,26 @@ const store = createStore(
     })
 );
 
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter(( expense ) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = true;
+
+        return startDateMatch && endDateMatch && textMatch;
+    });
+}
+
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses( state.expenses, state.filters );
+    console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch( addExpense({ description: 'Rent', amount: 100 }) );
-// const expenseTwo = store.dispatch( addExpense({ description: 'Horse', amount: 340 }) );
-//
+const expenseOne = store.dispatch( addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }) );
+const expenseTwo = store.dispatch( addExpense({ description: 'Horse', amount: 340, createdAt: -1000 }) );
+
 // store.dispatch( removeExpense({ id: expenseOne.expense.id }) );
 // store.dispatch( editExpense( expenseTwo.expense.id, { amount: 500 }) );
 //
@@ -147,8 +160,8 @@ store.subscribe(() => {
 // store.dispatch( sortByDate() );
 
 store.dispatch( setStartDate(125) );
-store.dispatch( setStartDate() );
-store.dispatch( setEndDate(700) );
+// store.dispatch( setStartDate() );
+// store.dispatch( setEndDate(700) );
 
 const demoState = {
     expenses: [{
