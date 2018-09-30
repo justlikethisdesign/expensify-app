f850258e3c0af290a968c7ea0804daa06ee7a77b
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-09-30 21:56:53 +0100

    Added sorting to visible expenses

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -135,9 +135,15 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter(( expense ) => {
         const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
         const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
-        const textMatch = true;
+        const textMatch = typeof text !== 'string' || expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if( sortBy === 'date'){
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if ( sortBy === 'amount'){
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 }
 
@@ -147,21 +153,21 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch( addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }) );
+const expenseOne = store.dispatch( addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }) );
 const expenseTwo = store.dispatch( addExpense({ description: 'Horse', amount: 340, createdAt: -1000 }) );
 
 // store.dispatch( removeExpense({ id: expenseOne.expense.id }) );
 // store.dispatch( editExpense( expenseTwo.expense.id, { amount: 500 }) );
 //
-// store.dispatch( setTextFilter('rent') );
+//store.dispatch( setTextFilter('e') );
 // store.dispatch( setTextFilter() );
 //
 // store.dispatch( sortByAmount() );
-// store.dispatch( sortByDate() );
+store.dispatch( sortByDate() );
 
-store.dispatch( setStartDate(125) );
+store.dispatch( setStartDate(0) );
 // store.dispatch( setStartDate() );
-// store.dispatch( setEndDate(700) );
+store.dispatch( setEndDate(1200) );
 
 const demoState = {
     expenses: [{
