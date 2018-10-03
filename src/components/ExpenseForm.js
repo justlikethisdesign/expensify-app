c72df16b8f8e1f9264391eba2f93d6310c919398
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-10-03 23:02:07 +0100

    Added regex for amount

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,7 +4,8 @@ export default class ExpenseForm extends React.Component {
 
     state = {
         description: '',
-        note: ''
+        note: '',
+        amount: ''
     }
 
     onDescriptionChange = (e) => {
@@ -15,6 +16,14 @@ export default class ExpenseForm extends React.Component {
     onNoteChange = (e) => {
         const note = e.target.note;
         this.setState(() => ({ note }));
+    }
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+
+        if ( amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState(() => ({ amount }));
+        }
     }
 
     render() {
@@ -29,8 +38,10 @@ export default class ExpenseForm extends React.Component {
                         onChange={this.onDescriptionChange}
                     />
                     <input
-                        type="number"
+                        type="text"
                         placeholder="amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
                     />
                     <textarea
                         placeholder="Add a note"
