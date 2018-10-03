8da93850c708d92c08d82ef748c32e84b0737ed1
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-10-03 22:33:12 +0100

    Added more text data

--- src/app.js
+++ src/app.js
@@ -13,13 +13,10 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch( addExpense({description: 'Water Bill',}));
-store.dispatch( addExpense({description: 'Gas Bill',}));
-store.dispatch( setTextFilter('bill') );
-
-setTimeout(() => {
-    store.dispatch( setTextFilter('rent') );
-}, 3000)
+store.dispatch( addExpense({description: 'Water Bill', amount: 4500}));
+store.dispatch( addExpense({description: 'Gas Bill', createdAt:1000}));
+store.dispatch( addExpense({description: 'Rent', amount: 109500}));
+store.dispatch( setTextFilter('water') );
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses( state.expenses, state.filters );
