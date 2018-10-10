63a718276557e3525e772c1b6f33ff6f7fa09f29
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-10-10 22:03:54 +0100

    Added tests for more NotFoundPage and DashBoardPage

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,13 +4,16 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpense from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
     <div>
-        <h1>Expense List</h1>
         {
-            props.expenses.map( (expense) => {
-                return <ExpenseListItem key={expense.id} {...expense}/>;
-            })
+            props.expenses.length === 0 ? (
+                <p>No Expenses</p>
+            ) : (
+                props.expenses.map( (expense) => {
+                    return <ExpenseListItem key={expense.id} {...expense}/>;
+                })
+            )
         }
     </div>
 );
