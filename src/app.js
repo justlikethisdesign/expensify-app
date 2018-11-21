00c248b0a40fc9f734309c0716402a5819ea7818
Rob Marshall <rob@fox.agency>
2018-11-21 16:39:04 +0000

    Added more reducers

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
@@ -16,18 +16,14 @@ import './firebase/firebase';
 
 const store = configureStore();
 
-store.dispatch( addExpense({description: 'Water Bill', amount: 4500}));
-store.dispatch( addExpense({description: 'Gas Bill', createdAt:1000}));
-store.dispatch( addExpense({description: 'Rent', amount: 109500}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses( state.expenses, state.filters );
-console.log( visibleExpenses );
-
 const jsx = (
     <Provider store = { store }>
         <AppRouter/>
     </Provider>
 )
 
-ReactDOM.render( jsx, document.getElementById('app') );
+ReactDOM.render( <p>Loading...</p>, document.getElementById('app') );
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render( jsx, document.getElementById('app') );
+});
