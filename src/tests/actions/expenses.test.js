0d73eba9387b950517d595a57f3a9cf42be08a12
Rob Marshall <rob@fox.agency>
2018-11-22 15:38:07 +0000

    Added "remove" functionality

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, startSetExpenses, setExpenses, startRemoveExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -116,6 +116,24 @@ test('should fetch expenses from firebase', (done) => {
             type: 'SET_EXPENSES',
             expenses
         });
+        done();
+    });
+});
+
+test('should remove expense from firebase', (done) => {
+    const store = createMockStore();
+    const expense = expenses[0];
+    store.dispatch(startRemoveExpense({ id: expense.id })).then(() => {
+
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id: expense.id
+        });
+        return database.ref(`expenses/${expense.id}`).once('value');
+
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(null);
         done();
     });
 });
