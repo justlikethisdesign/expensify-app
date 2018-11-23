45c8c7677e4345c4a592749ef3e19352039f98a9
Rob Marshall <rob@fox.agency>
2018-11-23 15:12:07 +0000

    Tidied login

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from "../components/Header";
-import { LoginPage } from "../components/LoginPage";
+import LoginPage from "../components/LoginPage";
 import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
 import AddExpensePage from "../components/AddExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
